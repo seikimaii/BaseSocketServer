7ba4ded2893d777148da4b6f9b493b8a3772c0ef
seikimaii <b145899987@gmail.com>
2023-01-18 16:26:06 +0800

    adding logging function and create new file when change day

--- BaseSocketServer.py
+++ BaseSocketServer.py
@@ -1,23 +1,49 @@
 import socket
 import time
 import select
+import logging
+import os
+from datetime import datetime
+import sys
 
 class BaseSocketServer(object):
-    def __init__(self):
-        self.__ip = None
-        self.__port = None
+    def __init__(self, ip:str="127.0.0.1", port:int=8050, 
+                 headerLength:int=10, idleTime:float=1):
+        '''idleTime is in seconds'''
+        self.__ip = ip
+        self.__port = port
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__headerLength = None
+        self.__headerLength = headerLength
         self.__commandDict = {}
-        self.__idleTime = None
+        self.__idleTime = idleTime
         self.__sock_list=[self.__socket]
         self.__clients={}
 
+        
+        self.__today_date = datetime.today()
+        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+        if not os.path.isdir('./Log'): os.mkdir('./Log')
+        handler = logging.FileHandler(f'./Log/{self.__today_date.date()}.log')
+        handler.setFormatter(formatter)
+        self.__logger = logging.getLogger("Server")
+        self.__logger.addHandler(handler)
+        self.__logger.setLevel(logging.DEBUG)
+    
+    def __updateHandler(self):
+        self.__logger.handlers[0].setStream(open(f'./Log/{datetime.today().date()}.log', 'a'))
+    
+    def __checkDate(self):
+        if self.__today_date != datetime.today().date():
+            self.__today_date = datetime.today().date()
+            self.__updateHandler()
+
     def run(self):
         self.__socket.listen()
+        self.__logger.info(f"server is working at IP:{self.__ip}, Port:{self.__port}")
         print(f"server is working at IP:{self.__ip}, port:{self.__port}")
 
         while True:
+            self.__checkDate()
             read_sock, output_sock, except_sock=select.select(self.__sock_list, [], self.__sock_list, 5.0)
 
             for notified_socket in read_sock:
@@ -43,7 +69,7 @@ class BaseSocketServer(object):
             for notified_socket in except_sock:
                 self.__sock_list.remove(notified_socket)
                 
-
+            time.sleep(self.__idleTime)
                 # print(notified_socket)
                 # print('sleeping....')
                 # time.sleep(5)
@@ -53,6 +79,19 @@ class BaseSocketServer(object):
             return func(para)
 
         return wrap
+    
+    
+    def setLogger(self, logger):
+        if isinstance(logger, logging.Logger):
+            self.__logger = logger
+        else:
+            raise TypeError("the logger must be Logger type!")
+    
+    def addHandler(self, handler):
+        if isinstance(handler, logging.Handler):
+            self.__logger.addHandler(handler)
+        else:
+            raise TypeError("handler must be logging.Handler type!")
 
     def __messageRecv(self, client_socket):
         try:
@@ -69,36 +108,36 @@ class BaseSocketServer(object):
 
 
     @property
-    def serverIdleTime(self):
+    def IdleTime(self):
         return self.__idleTime
     
-    @serverIdleTime.setter
-    def serverIdleTime(self, idleTime:float):
+    @IdleTime.setter
+    def IdleTime(self, idleTime:float):
         "idleTime:seconds"
         self.__idleTime = idleTime
     
     @property
-    def serverIP(self):
+    def IpAddress(self):
         return self.__ip
 
-    @serverIP.setter
-    def serverIP(self, ip:str):
+    @IpAddress.setter
+    def IpAddress(self, ip:str):
         self.__ip = ip
     
     @property
-    def serverPort(self):
+    def Port(self):
         return self.__port
 
-    @serverPort.setter
-    def serverPort(self, port:int):
+    @Port.setter
+    def Port(self, port:int):
         self.__port = port
 
     @property
-    def serverHeaderLength(self):
+    def HeaderLength(self):
         return self.__headerLength
 
-    @serverHeaderLength.setter
-    def serverHeaderLength(self, headerLength:int):
+    @HeaderLength.setter
+    def HeaderLength(self, headerLength:int):
         self.__headerLength = headerLength
 
     def Binding(self):
@@ -108,10 +147,10 @@ class BaseSocketServer(object):
 if __name__ == "__main__":
 
     server = BaseSocketServer()
-    server.serverHeaderLength = 10
-    server.serverIP = '127.0.0.1'
-    server.serverPort = 8888
-    server.serverIdleTime = 1
+    server.HeaderLength = 10
+    server.IpAddress = '127.0.0.1'
+    server.Port = 8888
+    server.IdleTime = 1
 
     @server.registCommand
     def hello(msg):
